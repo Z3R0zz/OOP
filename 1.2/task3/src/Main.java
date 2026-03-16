d7b0b3250954a0b4648ecf9d894d3e46b148f3dc
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2026-03-16 17:04:05 +0200

    module 1.2

--- 1.2/task3/src/Main.java
+++ 1.2/task3/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Weight (g):");
+        int grams = Integer.parseInt(scanner.nextLine());
+
+        double totalLuoti = grams / 13.28;
+
+        int leiviska = (int) (totalLuoti / (20 * 32));
+        double remainingLuoti = totalLuoti % (20 * 32);
+
+        int naula = (int) (remainingLuoti / 32);
+        double luoti = remainingLuoti % 32;
+
+        System.out.printf("%d grams is %d leiviskä, %d naula, and %.2f luoti. \n",
+                grams, leiviska, naula, luoti);
+
+        scanner.close();
     }
 }
