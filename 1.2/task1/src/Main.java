89d9f02e56534d4919c055242fe6659b0a0abcfe
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2026-03-16 16:31:22 +0200

    module 1.2, task 1

---
+++ 1.2/task1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give temperature in Fahrenheit");
+        float temp = Integer.parseInt(scanner.nextLine());
+
+        float Celsius = (temp - 32) * 5/9;
+        System.out.printf("Temperature in Celsius %.1f", Celsius);
+    }
+}
